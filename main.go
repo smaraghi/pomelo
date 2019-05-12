a2e1ebf225a54f5ddaa38601c9013e19209e36ca
Serven Maraghi <maraghiserven@gmail.com>
2019-05-11 20:22:38 -0400

    add file logging

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"syscall"
 	"time"
@@ -10,10 +11,18 @@ import (
 )
 
 func main() {
-	h := os.Getenv("HOME")
+	h := os.Getenv("HOME") + "/Downloads"
 	w := fs.Walk(h)
 	now := time.Now()
 	var files []string
+
+	log := os.Getenv("HOME") + "/.pomelo.log"
+	msg := fmt.Sprintf("\nNew Entry Date %s", now)
+	err := ioutil.WriteFile(log, []byte(msg), 0644)
+	if err != nil {
+		fmt.Println("Pomelo could not write to the log file.")
+		os.Exit(1)
+	}
 
 	for w.Step() {
 		if err := w.Err(); err != nil {
@@ -39,5 +48,7 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Number of files 30 days and older: %d\n", len(files))
+	numFiles := fmt.Sprintf("Number of files 30 days and older: %d\n", len(files))
+	fmt.Println(numFiles)
+
 }
